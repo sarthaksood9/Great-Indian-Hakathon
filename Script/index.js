b7da2d25eeb991e970508a71055f23ebaf480e04
Swapnil <swapniltayal422@gmail.com>
2022-04-23 02:02:24 +0530

    swapnil

---
+++ Script/index.js
@@ -0,0 +1,12 @@
+
+console.log(10)
+
+function login(){
+    document.getElementById("sectionC").style.display = "block"; 
+    document.getElementById("home").style.opacity = "10";
+}
+function close(){
+    console.log("10");
+    // document.getElementById("main").style.display = "none"; 
+    
+}
